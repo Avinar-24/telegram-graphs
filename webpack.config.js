e395a90d0c53102ce7bb451cf0b75de36b163a6b
Artyom Gospodarsky <art.gospodarsky@gmail.com>
2019-04-07 17:25:24 +0300

    Connect source maps to the project (#2)

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,25 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'source-map',
   plugins: [
     new HtmlWebPackPlugin({
       template: './assets/index.html',
       filename: './index.html',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['source-map-loader'],
+        enforce: 'pre',
+      },
+    ],
+  },
   resolve: {
     extensions: ['.js'],
   },
